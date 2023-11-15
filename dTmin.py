7b2475da94a11010471f7cf33e7d9734c4732241
cojre <corentin.jaire@epfl.ch>
2023-11-15 16:06:53 +0100

    fixing some issues

--- dTmin.py
+++ dTmin.py
@@ -10,18 +10,31 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from codes_01_energy_bill.coolprop_functions import mixture
 
-T1=333
-T2=348
+T1 = 333
+T2 = 348
 
-m1=0.48
-m2=0.55
-cp1=3400
-cp2=2976.18181818181
+p1 = 100000
+
+m1 = 0.48
+m2 = 0.55
+cp1 = 3400
+cp2 = 2976.18181818181
 
 
 slope1 = 1/(m1*cp1)
 slope2 = 1/(m2*cp2)
+
+# Enthalpy at T1 = 60°C for cream0
+State_1 = mixture(T=T1, P=p1, frac_water=0.65, frac_fat=0.35) # this is a dictionary!!
+h1 = State_1["enthalpy"]
+print(h1)
+
+# Enthalpy at T2 = 75°C for crpastb
+State_2 = mixture(T=T2, P=p1, frac_water=0.65, frac_fat=0.35) # this is a dictionary!!
+h2 = State_2["enthalpy"]
+print(h2)
 
 #Plot the two lines with T on the y-axis and h on the x-axis
 #h is the enthalpy of the stream between 1 and 1000 kJ/kg
