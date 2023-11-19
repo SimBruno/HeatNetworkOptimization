2d9b008e062fd20077b8763a56622256d581c657
Simon Bruno <simon.bruno@epfl.ch>
2023-11-19 11:07:22 +0100

    dTmin

--- dTmin.py
+++ dTmin.py
@@ -10,6 +10,9 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+#from codes_01_energy_bill.coolprop_functions import mixture
+
+
 
 T1 = 333
 T2 = 348
@@ -55,3 +58,76 @@ U = alpha/2
 dTmin = 0
 
 
+
+
+'''
+#
+
+T1= 60+273
+T2 = 75+273
+m1 = 0.48
+m2 = 0.55
+cp1 = 3470.44
+cp2 = 3482.19
+
+
+
+slope_cold = 1/(m1*cp1)
+slope_hot = 1/(m2*cp2) 
+
+
+#State_Past5=mixture(T=T2, P=100000, frac_water=0.5, frac_fat=0.5)
+#print(State_e6)
+h1=-91021
+h2=-38880.66                    
+
+T2_prime = T2 - slope_hot*h2
+print(T2_prime)                 
+
+
+#plot the two lines with T on the y-axis and h on the x-axis
+#h is the enthalpy of the stream between h1 and h2
+N = 100
+h = np.linspace(h1, h2, N)
+T = np.linspace(T1, T2, N)
+
+
+Tcold = T1 + h*slope_cold
+Thot = T2_prime + h*slope_hot  
+plt.plot(h, Tcold, label='Cold Stream')
+plt.plot(h, Thot, label='Hot Stream')
+plt.xlabel('Enthalpy (J/kg)')
+plt.ylabel('Temperature (K)')
+
+#Plot as a vertical line the dTmin (i.e. the minimum temperature difference between the two streams)
+dt = Thot-Tcold
+dTmin = dt.min()
+min_index = dt.argmin()
+plt.axvline(x=h[min_index], color='r', linestyle='--')
+
+plt.title('Temperature vs Enthalpy')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+print('dTmin = ', dTmin, 'K at h = ', h[min_index], 'kJ/kg')
+
+# Find T1_prime 
+
+T1_prime = T2 - dTmin
+print(f'T1_prime = {T1_prime} K')
+
+#Find the minimum Area of the HX 
+
+Q = 1
+LMTD = ((T2-T1_prime)-(T1-T2_prime))/(log((T2-T1_prime)/(T1-T2_prime)))
+alpha = 0
+U = alpha/2
+
+Area_min = Q/(U*LMTD)
+
+print(f'Area_min = {Area_min} m^2')
+
+#Find the CAPEX of the HX
+
+'''
